197fa675204dcd84725becf81e977adcba036d93
uzseek <260045220+uzseek@users.noreply.github.com>
2026-02-16 23:00:53 +0900

    feat: refactor article handling to store titles in Article struct

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 use scraper::{Html, Selector};
+struct Article {
+    title: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://news.ycombinator.com/";
     let response = reqwest::get(url).await?;
     let body = response.text().await?;
-
     let document = Html::parse_document(&body);
-
     let selector = Selector::parse(".titleline > a").unwrap();
+    let mut articles = Vec::new();
 
     for element in document.select(&selector) {
         let title = element.inner_html();
-        println!("{}", title);
+        let article = Article {
+            title,
+        };
+        articles.push(article);
+    }
+
+    for article in &articles {
+        println!("{}", article.title);
     }
 
     Ok(())
